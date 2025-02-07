7d7928d215381109ca0e7cabd84c5e0af3117bc1
cyriaque <cyriaquekr@gmail.com>
2025-02-07 22:49:38 +0100

    Ajout fichier configuration base de données

---
+++ server/database/database.connexion.js
@@ -0,0 +1,15 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+
+async function db_connection (){
+    const connexion = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
+    })
+    return connexion
+}
+export default  db_connection;
