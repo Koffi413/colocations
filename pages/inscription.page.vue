c13a02bf7540439ccf32944708dc2edaa8f0a9c8
cyriaque <cyriaquekr@gmail.com>
2025-02-08 11:39:49 +0100

    Creation des vues accueil et inscription mise en place d'un formulaire d'inscription

---
+++ pages/inscription.page.vue
@@ -0,0 +1,16 @@
+<template>
+    <form @submit.prevent="inscription">
+        <input type="text" id="fnom" v-model="credentials.username" placeholder="username"><br><br>
+        <input type="email" id="femail" v-model="credentials.email" placeholder="email"><br><br>
+        <input type="password" id="fpassword" v-model="credentials.password" placeholder="password"><br><br>
+        <button type="submit">S'inscrire</button>
+    </form>
+</template>
+<script setup>
+import { reactive } from 'vue'
+const infosUtilisateur = reactive({
+username: '',
+email: '',
+password: '',
+})
+</script>
